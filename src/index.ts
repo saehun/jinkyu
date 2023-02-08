586221eeb078e571c42607266d04104279ddbd89
minidonut <nycom13@gmail.com>
2023-02-08 16:48:37 +0900

    impl jinkyu

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello ts-app');
+#!/usr/bin/env node
+
+const jinkyu = [
+  '   d88b d888888b d8b   db db   dD db    db db    db ',
+  "   `8P'   `88'   888o  88 88 ,8P' `8b  d8' 88    88 ",
+  "    88     88    88V8o 88 88,8P    `8bd8'  88    88 ",
+  '    88     88    88 V8o88 88`8b      88    88    88 ',
+  'db. 88    .88.   88  V888 88 `88.    88    88b  d88 ',
+  "Y8888P  Y888888P VP   V8P YP   YD    YP    ~Y8888P' ",
+].join('\n');
+
+console.log(jinkyu);
